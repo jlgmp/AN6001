41c27fb1368d0366e47328c11094037d6ba10944
jlgmp <126658776+jlgmp@users.noreply.github.com>
2024-12-09 07:53:19 +0000

    1

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask import render_template,request
 import textblob
+import google.generativeai as genai
+import os
+api = os.getenv("makersuite")
+
+
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 app=Flask(__name__)
 @app.route("/",methods=["GET","POST"])
@@ -22,5 +29,15 @@ def SA_result():
     q=request.form.get("q")
     r = textblob.TextBlob(q).sentiment
     return(render_template("SA_result.html",r=r))
+
+@app.route("/Genai",methods=["GET","POST"])
+def Genai():
+    return(render_template("Genai.html"))
+
+@app.route("/Genai_result",methods=["GET","POST"])
+def Genai_result():
+    q=request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("Genai_result.html",r=r))
 if __name__ == "__main__":
     app.run(debug=True)
